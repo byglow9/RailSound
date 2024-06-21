10667a2bee885df72616a10e5b621367bf6b67be
glow <renancavenaghizuri@gmail.com>
2024-06-21 20:05:44 -0300

    additing wavesurfer

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "wavesurfer.js" # @7.7.15
